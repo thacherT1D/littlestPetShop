37657ad446bef0ffd54efdeeaee63aa5e6bd0193
THACHER <sophie.thacher@gmail.com>
2016-03-21 12:39:19 -0700

    read command built

--- pets.js
+++ pets.js
@@ -1,3 +1,25 @@
-'use strict'
-var a = 12;
-var b = 14;
+'use strict';
+
+var fs = require('fs');
+var path = require('path');
+var petsPath = path.join(__dirname, 'pets.json');
+
+var node = path.basename(process.argv[0]);
+var file = path.basename(process.argv[1]);
+var cmd = process.argv[2];
+
+if (cmd === 'read') {
+  fs.readFile(petsPath, 'utf8', function(err, data) {
+    if (err) {
+      throw err;
+    }
+
+    var pets = JSON.parse(data);
+
+    console.log(pets);
+  });
+}
+else {
+  console.error(`Usage: ${node} ${file} read`);
+  process.exit(1);
+}
