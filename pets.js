c81947c9cd9932f7d189b4b29da2a889e206b524
THACHER <sophie.thacher@gmail.com>
2016-03-21 14:11:06 -0700

    index function complete

--- pets.js
+++ pets.js
@@ -7,16 +7,25 @@ var petsPath = path.join(__dirname, 'pets.json');
 var node = path.basename(process.argv[0]);
 var file = path.basename(process.argv[1]);
 var cmd = process.argv[2];
+var index = process.argv[3];
+
+
 
 if (cmd === 'read') {
   fs.readFile(petsPath, 'utf8', function(err, data) {
     if (err) {
       throw err;
     }
-
     var pets = JSON.parse(data);
+    if (index >= 0 && index < pets.length) {
+      console.log(pets[index]);
+      // console.log(pets);
+    }
+    else {
+      console.error(`Usage: ${node} ${file} read INDEX`);
+      process.exit(1);
+    }
 
-    console.log(pets);
   });
 }
 else {
