1ce729a36944a93561f23963c129c2d0e680910c
THACHER <sophie.thacher@gmail.com>
2016-03-21 14:41:12 -0700

    create working

--- pets.js
+++ pets.js
@@ -3,13 +3,9 @@
 var fs = require('fs');
 var path = require('path');
 var petsPath = path.join(__dirname, 'pets.json');
-
 var node = path.basename(process.argv[0]);
 var file = path.basename(process.argv[1]);
 var cmd = process.argv[2];
-var index = process.argv[3];
-
-
 
 if (cmd === 'read') {
   fs.readFile(petsPath, 'utf8', function(err, data) {
@@ -17,18 +13,51 @@ if (cmd === 'read') {
       throw err;
     }
     var pets = JSON.parse(data);
-    if (index >= 0 && index < pets.length) {
+    var index = process.argv[3];
+
+    if(!index) {
+      console.log(pets);
+      process.exit(1);
+    }
+
+    else if (index >= 0 && index < pets.length) {
       console.log(pets[index]);
-      // console.log(pets);
     }
     else {
       console.error(`Usage: ${node} ${file} read INDEX`);
       process.exit(1);
     }
+  });
+}
+else if (cmd === 'create') {
+  fs.readFile(petsPath, 'utf8', function(readErr, data) {
+    if (readErr) {
+      throw readErr;
+    }
+    var pets = JSON.parse(data);
+    var petAge = process.argv[3];
+    var petKind = process.argv[4];
+    var petName = process.argv[5];
+
+    if (!petAge || !petKind || !petName) {
+      console.error(`Usage: ${node} ${file} ${cmd} AGE KIND NAME`);
+      process.exit(1);
+    }
+
+    pets.push({age: petAge, kind: petKind, name: petName});
+    console.log(pets);
+    // var petsJSON = JSON.stringify(pets);
+
+    // fs.writeFile(petsPath, petsJSON, function(writeErr) {
+    //   if (writeErr) {
+    //     throw writeErr;
+    //   }
+    // console.log(pets);
+    // });
 
   });
 }
 else {
-  console.error(`Usage: ${node} ${file} read`);
+  console.error(`Usage: ${node} ${file} [read | create]`);
   process.exit(1);
 }
