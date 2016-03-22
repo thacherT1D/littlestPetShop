1214ad0a0d310d850a09dfdc0e6b308f4ccc6888
THACHER <sophie.thacher@gmail.com>
2016-03-22 14:19:41 -0700

    working es6 version

--- pets.js
+++ pets.js
@@ -1,22 +1,24 @@
-//FOR shebag challenge
-//#!/usr/bin/env node
+// FOR shebag challenge
+// #!/usr/bin/env node
 // chmod +x pets.js
+
 'use strict';
 
-var fs = require('fs');
-var path = require('path');
-var petsPath = path.join(__dirname, 'pets.json');
-var node = path.basename(process.argv[0]);
-var file = path.basename(process.argv[1]);
-var cmd = process.argv[2];
+const fs = require('fs');
+const path = require('path');
+const petsPath = path.join(__dirname, 'pets.json');
+const node = path.basename(process.argv[0]);
+const file = path.basename(process.argv[1]);
+const cmd = process.argv[2];
+let petAge2 = 0;
 
 if (cmd === 'read') {
-  fs.readFile(petsPath, 'utf8', function (err, data) {
+  fs.readFile(petsPath, 'utf8', (err, data) => {
     if (err) {
       throw err;
     }
-    var pets = JSON.parse(data);
-    var index = process.argv[3];
+    const pets = JSON.parse(data);
+    const index = process.argv[3];
 
     if (!index) {
       console.log(pets);
@@ -29,51 +31,51 @@ if (cmd === 'read') {
     }
   });
 } else if (cmd === 'create') {
-  fs.readFile(petsPath, 'utf8', function(readErr, data) {
+  fs.readFile(petsPath, 'utf8', (readErr, data) => {
     if (readErr) {
       throw readErr;
     }
-    var pets = JSON.parse(data);
-    var petAge = process.argv[3];
-    var petKind = process.argv[4];
-    var petName = process.argv[5];
+    const pets = JSON.parse(data);
+    const petAge = process.argv[3];
+    const petKind = process.argv[4];
+    const petName = process.argv[5];
 
     if (!petAge || !petKind || !petName) {
       console.error(`Usage: ${node} ${file} ${cmd} AGE KIND NAME`);
       process.exit(1);
     }
 
-    petAge = parseInt(petAge, 10);
-    pets.push({ age: petAge, kind: petKind, name: petName });
+    petAge2 = parseInt(petAge, 10);
+    pets.push({ age: petAge2, kind: petKind, name: petName });
 
-    var petsJSON = JSON.stringify(pets);
+    const petsJSON = JSON.stringify(pets);
 
-    fs.writeFile(petsPath, petsJSON, function (writeErr) {
+    fs.writeFile(petsPath, petsJSON, (writeErr) => {
       if (writeErr) {
         throw writeErr;
       }
     });
   });
 } else if (cmd === 'update') {
-  fs.readFile(petsPath, 'utf8', function (readErr, data) {
+  fs.readFile(petsPath, 'utf8', (readErr, data) => {
     if (readErr) {
       throw readErr;
     }
-    var pets = JSON.parse(data);
-    var index = process.argv[3];
-    var petAge = process.argv[4];
-    var petKind = process.argv[5];
-    var petName = process.argv[6];
+    const pets = JSON.parse(data);
+    const index = process.argv[3];
+    const petAge = process.argv[4];
+    const petKind = process.argv[5];
+    const petName = process.argv[6];
 
     if ((!index || !petAge || !petKind || !petName) || ((index >= 0 && index < pets.length) !== true)) {
       console.error(`Usage: ${node} ${file} ${cmd} INDEX AGE KIND NAME`);
       process.exit(1);
     }
-    petAge = parseInt(petAge, 10);
-    pets[index] = ({ age: petAge, kind: petKind, name: petName });
-    var petsJSON = JSON.stringify(pets);
+    petAge2 = parseInt(petAge, 10);
+    pets[index] = ({ age: petAge2, kind: petKind, name: petName });
+    const petsJSON = JSON.stringify(pets);
 
-    fs.writeFile(petsPath, petsJSON, function (writeErr) {
+    fs.writeFile(petsPath, petsJSON, (writeErr) => {
       if (writeErr) {
         throw writeErr;
       }
@@ -81,22 +83,22 @@ if (cmd === 'read') {
     });
   });
 } else if (cmd === 'destroy') {
-  fs.readFile(petsPath, 'utf8', function (readErr, data) {
+  fs.readFile(petsPath, 'utf8', (readErr, data) => {
     if (readErr) {
       throw readErr;
     }
-    var pets = JSON.parse(data);
-    var index = process.argv[3];
+    const pets = JSON.parse(data);
+    const index = process.argv[3];
 
-    if (!index) {
+    if (!index || ((index >= 0 && index < pets.length) !== true)) {
       console.error(`Usage: ${node} ${file} ${cmd} INDEX`);
       process.exit(1);
     }
 
     pets.splice(index, 1);
-    var petsJSON = JSON.stringify(pets);
+    const petsJSON = JSON.stringify(pets);
 
-    fs.writeFile(petsPath, petsJSON, function (writeErr) {
+    fs.writeFile(petsPath, petsJSON, (writeErr) => {
       if (writeErr) {
         throw writeErr;
       }
