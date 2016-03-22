fdfe462ea473860c1c72c9b6835748319a07f89d
THACHER <sophie.thacher@gmail.com>
2016-03-22 13:14:55 -0700

    working model pre-bonuses

--- server.js
+++ server.js
@@ -1,36 +1,31 @@
-// $(document).ready(function() {
-//   'use strict';
-//
-//
-// });
+'use strict';
 
-
+var fs = require('fs');
+var path = require('path');
+var petsPath = path.join(__dirname, 'pets.json');
+var pets;
 var http = require('http');
-// var routes = require('./routes');
+
+fs.readFile(petsPath, 'utf8', function (err, data) {
+  if (err) {
+    throw err;
+  }
+  pets = JSON.parse(data);
+});
 
 var handleRequest = function(req, res) {
   // res.setHeader("Content-Type", "text/plain");
   // res.end("Hi, I'll take two packets of sugar");
   if(req.url === '/pets') {
-    res.setHeader("Content-Type", "application/json");
     res.statusCode = 200;
-    res.setEncoding('utf8');
-    // res.end("/pets");
-
-    $.ajax({
-      url: '/pets.json',
-      method: 'GET',
-      success: function(data) {
-        console.log(data);
-      }
-    });
-
+    res.end(JSON.stringify(pets));
 
   } else if (req.url === '/pets/0') {
-
-    res.end('/pets/0');
+    // res.end('/pets/0');
+    res.end(JSON.stringify(pets[0]));
   } else if (req.url === '/pets/1') {
-    res.end('/pets/1');
+    // res.end('/pets/1');
+    res.end(JSON.stringify(pets[1]));
   } else if (req.url === '/pets/2') {
     res.setHeader("Content-Type", "text/plain");
     res.statusCode = 404;
