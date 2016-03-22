55e64d69f7b3e38c9c88f71d46fb5cada1986aa1
THACHER <sophie.thacher@gmail.com>
2016-03-22 14:33:20 -0700

    es6 lint complete

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 const fs = require('fs');
 const path = require('path');
 const petsPath = path.join(__dirname, 'pets.json');
-const pets;
+let pets = [];
 const http = require('http');
 
 fs.readFile(petsPath, 'utf8', (err, data) => {
@@ -13,32 +13,23 @@ fs.readFile(petsPath, 'utf8', (err, data) => {
   pets = JSON.parse(data);
 });
 
-var handleRequest = (req, res) => {
-  // res.setHeader("Content-Type", "text/plain");
-  // res.end("Hi, I'll take two packets of sugar");
+const handleRequest = (req, res) => {
   if (req.url === '/pets') {
-    res.statusCode = 200;
+    // res.statusCode = 200;
     res.end(JSON.stringify(pets));
-  }
-  else if (req.url === '/pets/0') {
-    // res.end('/pets/0');
+  } else if (req.url === '/pets/0') {
     res.end(JSON.stringify(pets[0]));
-  }
-  else if (req.url === '/pets/1') {
-    // res.end('/pets/1');
+  } else if (req.url === '/pets/1') {
     res.end(JSON.stringify(pets[1]));
-  }
-  else if (req.url === '/pets/2') {
+  } else if (req.url === '/pets/2') {
     res.setHeader('Content-Type', 'text/plain');
-    res.statusCode = 404;
+    // res.statusCode = 404;
     res.end('Not Found');
-  }
-  else if (req.url === '/pets/-1') {
+  } else if (req.url === '/pets/-1') {
     res.setHeader('Content-Type', 'text/plain');
-    res.statusCode = 404;
+    // res.statusCode = 404;
     res.end('Not Found');
-  }
-  else {
+  } else {
     res.end(req.url);
   }
 };
@@ -46,7 +37,6 @@ var handleRequest = (req, res) => {
 const server = http.createServer(handleRequest);
 
 const port = process.env.PORT || 5000;
-server.listen(port, function() {
+server.listen(port, () => {
   console.log('Listening...');
-
 });
