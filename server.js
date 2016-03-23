599badbf8881722c50ace84311d0604dd32cd34d
THACHER <sophie.thacher@gmail.com>
2016-03-23 15:24:32 -0700

    Node Server Version done-ish

--- server.js
+++ server.js
@@ -6,64 +6,66 @@ const petsPath = path.join(__dirname, 'pets.json');
 let pets = [];
 const http = require('http');
 
-fs.readFile(petsPath, 'utf8', (err, data) => {
-  if (err) {
-    throw err;
-  }
-  pets = JSON.parse(data);
-});
-
 const handleRequest = (req, res) => {
   const petRegExp = /^\/pets\/(.*)$/;
   const matches = req.url.match(petRegExp);
 
-  if (req.url === '/pets' && req.method === 'POST') {
-    let body = [];
-    req.on('data', (chunk) => {
-    body.push(chunk);
-    // console.log(`BODY: ${chunk}`);
-  }).on('end',() => {
-    body = Buffer.concat(body).toString();
-    body = JSON.parse(body);
-    pets.push(body);
-    const petsString = JSON.stringify(pets);
+  fs.readFile(petsPath, 'utf8', (err, data) => {
+    pets = JSON.parse(data);
 
-    fs.writeFile(petsPath, petsString, (err, data) => {
+    if (req.url === '/pets' && req.method === 'POST') {
       if (err) {
         throw err;
       }
-    })
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "application/json");
-    res.end(JSON.stringify(body));
-  })
+      // console.log(data);
 
-    console.log("post request made");
-    console.log(process.argv);
-    console.log(req.data);
-    res.end("JSON.stringify(pets)");
+      let body = '';
+      req.on('data', function (chunk) {
+        body += chunk.toString();
+      });
+      req.on('end', () => {
+        if(body !== '') {
+          req.body = JSON.parse(body);
+        }
 
-  }
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "application/json");
 
-  else if (req.url === '/pets' && req.method === 'GET') {
-    res.statusCode = 200;
-    res.end(JSON.stringify(pets));
-    // console.log(req.method);
-    // console.log(req.body);
-  } else if (matches) {
-    const id = matches[1];
-    if (id >= 0 && id < pets.length) {
-      res.statusCode = 200;
-      res.end(JSON.stringify(pets[id]));
-    } else {
-      res.statusCode = 404;
-      res.end('No Pet At that ID');
+        pets.push(req.body);
+
+        const petsString = JSON.stringify(pets);
+
+        fs.writeFile(petsPath, petsString, (err, data) => {
+          if (err) {
+            throw err;
+          }
+          else {
+            res.end(JSON.stringify(pets));
+          }
+        })
+      });
     }
-  } else {
-    res.statusCode = 404;
-    res.end('Not Found');
-  }
-};
+
+    else if (req.url === '/pets' && req.method === 'GET') {
+      res.statusCode = 200;
+      res.end(JSON.stringify(pets));
+    }
+    else if (matches) {
+      const id = matches[1];
+      if (id >= 0 && id < pets.length) {
+        res.statusCode = 200;
+        res.end(JSON.stringify(pets[id]));
+      } else {
+        res.statusCode = 404;
+        res.end('No Pet At that ID');
+      }
+    }
+    else {
+      res.statusCode = 404;
+      res.end('Not Found');
+    }
+  });
+}
 
 const server = http.createServer(handleRequest);
 
