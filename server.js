fdd46a16e6add895da5e32075c8e19064b07f0ef
THACHER <sophie.thacher@gmail.com>
2016-03-23 10:46:27 -0700

    POST statment drafted, not confirmed working yet

--- server.js
+++ server.js
@@ -14,22 +14,51 @@ fs.readFile(petsPath, 'utf8', (err, data) => {
 });
 
 const handleRequest = (req, res) => {
-  var petRegExp = /^\/pets\/(.*)$/;
-  var matches = req.url.match(petRegExp);
+  const petRegExp = /^\/pets\/(.*)$/;
+  const matches = req.url.match(petRegExp);
 
+  if (req.url === '/pets' && req.method === 'POST') {
+    let body = [];
+    req.on('data', (chunk) => {
+    body.push(chunk);
+    // console.log(`BODY: ${chunk}`);
+  }).on('end',() => {
+    body = Buffer.concat(body).toString();
+    body = JSON.parse(body);
+    pets.push(body);
+    const petsString = JSON.stringify(pets);
 
-  if (req.url === '/pets') {
+    fs.writeFile(petsPath, petsString, (err, data) => {
+      if (err) {
+        throw err;
+      }
+    })
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "application/json");
+    res.end(JSON.stringify(body));
+  })
+
+    console.log("post request made");
+    console.log(process.argv);
+    console.log(req.data);
+    res.end("JSON.stringify(pets)");
+
+  }
+
+  else if (req.url === '/pets' && req.method === 'GET') {
     res.statusCode = 200;
     res.end(JSON.stringify(pets));
+    // console.log(req.method);
+    // console.log(req.body);
   } else if (matches) {
-      var id = matches[1];
-      if(id >= 0 && id < pets.length){
-        res.statusCode = 200;
-        res.end(JSON.stringify(pets[id]));
-      } else {
-        res.statusCode = 404;
-        res.end('No Pet At that ID');
-      }
+    const id = matches[1];
+    if (id >= 0 && id < pets.length) {
+      res.statusCode = 200;
+      res.end(JSON.stringify(pets[id]));
+    } else {
+      res.statusCode = 404;
+      res.end('No Pet At that ID');
+    }
   } else {
     res.statusCode = 404;
     res.end('Not Found');
