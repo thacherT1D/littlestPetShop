0a2c6551faacc6a774174a27a833d0beebd4f5d5
THACHER <sophie.thacher@gmail.com>
2016-03-23 20:32:15 -0700

    complete in es6 and linted

--- server.js
+++ server.js
@@ -4,36 +4,31 @@ const app = express();
 const fs = require('fs');
 const path = require('path');
 const petsPath = path.join(__dirname, 'pets.json');
-const http = require('http');
 
 fs.readFile(petsPath, 'utf8', (err, data) => {
   const pets = JSON.parse(data);
-  // console.log(pets);
-
   app.post('/pets', (req, res) => {
-
     var body = '';
-    req.on('data', function (chunk) {
+    req.on('data', (chunk) => {
       body += chunk.toString();
     });
     req.on('end', () => {
-      if(body !== '') {
+      if (body !== '') {
         req.body = JSON.parse(body);
       }
 
       if (!(req.body.age) || !(req.body.kind) || !(req.body.name) || !(Number.isInteger(parseInt(req.body.age)))) {
         res.status(400);
         res.send('Bad Request');
-      }
-      else {
+      } else {
         res.statusCode = 200;
-        res.setHeader("Content-Type", "application/json");
+        res.setHeader('Content-Type', 'application/json');
 
-        newAnimal = {
+        const newAnimal = {
           age: parseInt(req.body.age),
           kind: req.body.kind,
-          name: req.body.name
-        }
+          name: req.body.name,
+        };
 
         pets.push(newAnimal);
       }
@@ -43,8 +38,7 @@ fs.readFile(petsPath, 'utf8', (err, data) => {
       fs.writeFile(petsPath, petsString, (err, data) => {
         if (err) {
           throw err;
-        }
-        else {
+        } else {
           res.send(petsString);
         }
       });
@@ -56,87 +50,18 @@ fs.readFile(petsPath, 'utf8', (err, data) => {
   });
 
   app.get('/pets/:id', (req, res) => {
-    var givenId = req.params.id;
-    if(givenId >= 0 && givenId < pets.length){
-      var currentPet = pets[givenId];
+    const givenId = req.params.id;
+    if (givenId >= 0 && givenId < pets.length) {
+      const currentPet = pets[givenId];
       res.status(200);
       res.send(currentPet);
-    }
-    else {
+    } else {
       res.status(404);
       res.send('Not found');
     }
   });
 });
 
-app.listen(5000, function () {
-  console.log("Go to localhost:5000/");
+app.listen(5000, () => {
+  console.log('Go to localhost:5000/');
 });
-
-//////// below node version
-//
-//
-//
-//   fs.readFile(petsPath, 'utf8', (err, data) => {
-//     pets = JSON.parse(data);
-//
-//     if (req.url === '/pets' && req.method === 'POST') {
-//       if (err) {
-//         throw err;
-//       }
-//       // console.log(data);
-//
-//       let body = '';
-//       req.on('data', function (chunk) {
-//         body += chunk.toString();
-//       });
-//       req.on('end', () => {
-//         if(body !== '') {
-//           req.body = JSON.parse(body);
-//         }
-//
-//         res.statusCode = 200;
-//         res.setHeader("Content-Type", "application/json");
-//
-//         pets.push(req.body);
-//
-//         const petsString = JSON.stringify(pets);
-//
-//         fs.writeFile(petsPath, petsString, (err, data) => {
-//           if (err) {
-//             throw err;
-//           }
-//           else {
-//             res.end(JSON.stringify(pets));
-//           }
-//         })
-//       });
-//     }
-//
-//     else if (req.url === '/pets' && req.method === 'GET') {
-//       res.statusCode = 200;
-//       res.end(JSON.stringify(pets));
-//     }
-//     else if (matches) {
-//       const id = matches[1];
-//       if (id >= 0 && id < pets.length) {
-//         res.statusCode = 200;
-//         res.end(JSON.stringify(pets[id]));
-//       } else {
-//         res.statusCode = 404;
-//         res.end('No Pet At that ID');
-//       }
-//     }
-//     else {
-//       res.statusCode = 404;
-//       res.end('Not Found');
-//     }
-//   });
-// }
-//
-// const server = http.createServer(handleRequest);
-//
-// const port = process.env.PORT || 5000;
-// server.listen(port, () => {
-//   console.log('Listening...');
-// });
